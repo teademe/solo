b6c8c29e6123a30ca275c618ce5d16b6f75f4402
teademe <thea_pls@yahoo.com>
2023-02-16 22:12:06 -0500

    solo commit

---
+++ solo1/solo1.py
@@ -0,0 +1,230 @@
+# print(60)            #The line below adds two numbers
+                    #  //Calculate the number of hours in a week
+
+# print("Welcome to the Code Playground")
+# print("Coding is your new superpower")
+
+# print(9 + 4 - 7)
+# print(12 * 12)
+# print(8 / 2)
+# print(18*3+7*1)
+# print(47+14)
+
+# print(2.71)
+# print("October")
+
+# print("I am a string")
+
+# print("Theme " + "Park")
+# print("water " + "park")
+
+
+# print("Spider", "Man", sep="-")
+# print("Iron", "Man", sep ="-")
+# print("Real", "madrid", sep = "-" )
+
+
+
+# print("28"+"6")
+
+# print("100")     string because it has ""
+
+# print("Knowledge " + "is " + "power")
+
+# A whole number:  INTEGER
+# A number with a decimal point:  FLOAT
+# A piece of text:  STRING
+
+
+# print("Name:")
+# print("Mary")
+# print("Balance:")
+# print(500)
+
+# print("Balance:", 5000)
+
+
+
+                      # variable=value
+
+# points=15
+# print(points)
+
+
+# EXAMPLE
+# x = 42       #integer
+# y = "Hello"  #string
+# z = 3.14     #float
+
+# print(x)
+# print(y)
+# print(z)
+
+# price= 15
+# print(price*3)
+
+# year = 2023
+# print(year + 5)
+
+# x = 8
+# print(x)
+# x = 41
+# print(x)
+
+# age = 42
+# age = 24
+# print(age)
+
+# x = 9
+# y = 5
+# print(x*y)
+# print(x/y)
+
+# result = 798
+# currency = "$"
+# print("Total:", currency, result)
+
+# money = 200
+# currency = "€"
+# print("Balance:", currency, money)
+
+# num = 4
+# print(num*5)
+
+# print(7425/550)
+
+# x = input("My name is:")
+# print(x)
+
+# number = input(7)
+# print("You entered: " + number)
+
+# print("your name is:" + "7")
+
+# name = input("Tea")
+# print("Welcome, " + name)
+
+# print("welcome, " "Tea")
+
+# txt = input("hi")
+# print(txt)
+
+# name = input("tea")
+# age =input(42)
+# print(name + "is" + age +"years")
+
+# age = int(input(15))
+# print(age)
+
+
+# x = "2"
+# y = "4"
+# z = int(x) + int(y)
+# print(z)
+
+# height = float(input(14))
+# print(height) 
+
+# age = 42
+# print("his age is " + str(age))
+
+# a = 5
+# b = 9
+# x = str(a)
+# y = str(b)
+# print(x+y)
+
+# name = input("tea")
+# age = input(42)
+# print(name + " is " + age)
+
+# x =input("a")
+# y =int(input(8))
+# print(x *y)
+
+# width = input(5)
+# height = input(4)
+# area = int(width) * int(height)
+# print(area//9)
+
+
+                    #    In-place operators let you write code like 'x = x + 3' more concisely, as 'x += 3'.
+# x = 2
+# print(x)
+# x += 3
+# print(x)
+
+# x = 4
+# x *= 3
+# print(x)
+
+                    #  In-place operators can be used for any numerical operation (+, -, *, /, %, **, //).
+
+# x = 8
+# x -= 2
+# print(x)
+
+                #   The solution to the problem is 
+                # 👇🏼 x = 9 x %= 2 x += 3 
+                # How many times can 9 be divisible by 2?
+                #  4 times remainder 1, so x = 1 
+                # Now x = 1 is introduced into the 
+                # last print statement as 1+=3 = 4 
+                # There you have your answer ✅ # 
+# x = 9              
+# x %= 2
+# x += 3
+# print(x)
+
+
+
+# x = "spam"
+# print(x)
+# x += "eggs"
+# print(x)
+
+# x = "a"
+# x *= 3
+# print(x)
+
+
+                  # wrong
+# miles = int(input())
+# km = miles * 1.60934
+
+# print(km)
+
+                #    right
+# miles = int(55)       
+# km = miles * 1.60934
+# print(km)
+
+
+# spam = "7"
+# spam = spam + "0"
+# eggs = int(spam) + 3
+# print(float(eggs))
+
+
+# age = 42
+# print(age+8)
+
+# x = 5
+# y = x + 3
+# y = int(str(y) + "2")
+# print(y)
+
+
+# x=4
+# x+=5
+# print(x)
+
+# x = 3
+# num = 17
+# print(num % x)
+
+bill=50
+tip=bill *20 
+y=tip / 100
+
+print(y)
