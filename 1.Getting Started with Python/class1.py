25d9d38b2a2553ba23020d1eb4f6ffcd4f0d929a
teademe <thea_pls@yahoo.com>
2023-02-20 20:58:26 -0500

    new solo commit

---
+++ 1.Getting Started with Python/class1.py
@@ -0,0 +1,97 @@
+# print(60)            #The line below adds two numbers
+                    #  //Calculate the number of hours in a week
+
+# print("Welcome to the Code Playground")
+# print("Coding is your new superpower")
+
+# print(9 + 4 - 7)
+# print(12 * 12)
+# print(8 / 2)
+# print(18*3+7*1)
+# print(47+14)
+
+# print(2.71)
+# print("October")
+
+# print("I am a string")
+
+# print("Theme " + "Park")
+# print("water " + "park")
+
+
+# print("Spider", "Man", sep="-")
+# print("Iron", "Man", sep ="-")
+# print("Real", "madrid", sep = "-" )
+
+
+
+# print("28"+"6")
+
+# print("100")     string because it has ""
+
+# print("Knowledge " + "is " + "power")
+
+# A whole number:  INTEGER
+# A number with a decimal point:  FLOAT
+# A piece of text:  STRING
+
+
+# print("Name:")
+# print("Mary")
+# print("Balance:")
+# print(500)
+
+# print("Balance:", 5000)
+
+
+
+                      # variable=value
+
+# points=15
+# print(points)
+
+
+# EXAMPLE
+# x = 42       #integer
+# y = "Hello"  #string
+# z = 3.14     #float
+
+# print(x)
+# print(y)
+# print(z)
+
+# price= 15
+# print(price*3)
+
+# year = 2023
+# print(year + 5)
+
+# x = 8
+# print(x)
+# x = 41
+# print(x)
+
+# age = 42
+# age = 24
+# print(age)
+
+# x = 9
+# y = 5
+# print(x*y)
+# print(x/y)
+
+# result = 798
+# currency = "$"
+# print("Total:", currency, result)
+
+# money = 200
+# currency = "€"
+# print("Balance:", currency, money)
+
+# num = 4
+# print(num*5)
+
+# print(7425/550)
+
+
+
---
+++ 3.Control Flow/Else statment/class1.py
@@ -0,0 +1,83 @@
+# The else statement can be used to run some statements when the condition of the if statement is False.
+
+# x = 4
+# if x == 5:
+#     print("yes")
+# else:
+#     print("No")
+
+# if 1 + 1 == 2:
+#     if 2 * 2 == 8:
+#         print("if")
+#     else:
+#         print("else")   ////////
+
+
+# The colon after the else keyword is important, don’t leave it out.
+
+
+# grade = 87
+# if grade>=75:
+#     print("Passed!")
+# else: 
+#     print("Failed")
+
+
+# Every if condition block can have only one else statement.
+# So for us to make multiple checks, we need to chain if and else statements.
+# EXAMPLE DOWN
+
+# num = 3
+# if num == 1:
+#     print("One")
+# else:
+#     if num == 2:
+#         print("Two")
+#     else:
+#         if num == 3:
+#             print("Three")
+#         else:
+#             print("Something else")
+
+
+# x = 10
+# y = 20
+# if x > y:
+#     print("If statement")
+# else:
+#     print("Else statement")
+
+
+# Too many if/else statements make your code long and hard to read.
+# The best way to solve this is the elif (short for else if) statement. 
+# It’s a shortcut to use when chaining together if and else statements, making the code shorter and easier to read.
+# The same example from the previous part can be rewritten using elif statements
+
+
+# num = 8
+# if num == 1:
+#     print("One")
+# elif num ==2:
+#     print("Two")
+# elif num ==3:
+#     print("Three")
+# else:
+#     print("Something else")
+
+
+# num = 42
+# if num <0:
+#     print("Negative")
+# elif num > 0:
+#     print("Positive")
+# else:
+#     print("Zero")
+
+
+# x = 10
+# y = 20
+# if x > y:
+#     print(x+y)
+# else:
+#     print(y-x)
+
